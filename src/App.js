66d4080fb6550af28c1f7fb68d23c28fd86609c5
YasmeenD <82047222+yasditta@users.noreply.github.com>
2022-10-11 17:24:57 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import HomePage from './pages/home';
+import SearchPage from './pages/search';
+import PetDetailsPage from './pages/detail';
+import PetDetailsNotFound from './pages/petDetailsNotFound';
+import Navigation from './components/navigation';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <Route path="/:type">
+        <Navigation />
+      </Route>
+
+      <Switch>
+        <Route path="/search">
+          <SearchPage />
+        </Route>
+
+        <Route path="/:type/:id">
+          <PetDetailsPage />
+        </Route>
+
+        <Route path="/pet-details-not-found">
+          <PetDetailsNotFound />
+        </Route>
+
+        <Route path="/:type?">
+          <HomePage />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
